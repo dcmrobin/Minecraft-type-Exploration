85fe17212dc4900868a7bd6dd49ccc809a2a01a1
DavidCM <dcm.robin@gmail.com>
2024-09-18 15:20:22 +0100

    gonna transfer some stuff to jobs

--- Assets/Voxels/Scripts/Chunk.cs
+++ Assets/Voxels/Scripts/Chunk.cs
@@ -1,6 +1,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Unity.Jobs;
+using Unity.Burst;
+using Unity.Collections;
 using System.Diagnostics;
 
 public class Chunk : MonoBehaviour
